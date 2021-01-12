2de57b9647a407f63cec998aa086fad915b2977f
José Ignacio Escribano <jiep12345@hotmail.com>
2021-01-12 20:40:24 +0100

    Add version number

--- src/hello-world.c
+++ src/hello-world.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+#ifndef VERSION
+#define VERSION "1.0.0"
+#endif
+
+void banner(){
+  printf("Welcome to Hello v%s\n", VERSION);
+}
+
 int main(int argc, char const *argv[]) {
+  banner();
 
   if (argc != 2) {
     printf("You must provide a parameter!\n");
