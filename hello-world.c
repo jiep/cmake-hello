5fe935c907484d5a003623decdcc3d72d69dda91
José Ignacio Escribano <jiep12345@hotmail.com>
2021-01-12 20:05:07 +0100

    Add custom parameter

--- hello-world.c
+++ hello-world.c
@@ -2,7 +2,12 @@
 
 int main(int argc, char const *argv[]) {
 
-  printf("Hello World!\n");
+  if (argc != 2) {
+    printf("You must provide a parameter!\n");
+    return 1;
+  }
+
+  printf("Hello, %s!\n", argv[1]);
 
   return 0;
 }
